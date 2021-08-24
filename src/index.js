26d0a69a1d1447a0d465ba83b2fce10242fe6846
Ig0r0k <gigorasd@gmail.com>
2021-08-25 00:24:37 +0300

    fix: fix correct work on hard 28 test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ function eval() {
 function expressionCalculator(expr) {
     let begin = 0;
     let end = 0;
+    let divZero = false;
     for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === '/' && expr[i + 2] === '0') divZero = true;
         if (expr[i] === '(') begin++;
         else if (expr[i] === ')') end++;
     }
@@ -15,7 +17,7 @@ function expressionCalculator(expr) {
     } else {
         let a = new Function(`return ${expr}`);
         let b = a();
-        if (b === Infinity) {
+        if (b === Infinity || divZero) {
             throw new Error('TypeError: Division by zero.');
         }
         return b;
