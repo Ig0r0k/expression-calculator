4f91ff1a76ee524f3c2262f26cd28587c45f4685
Ig0r0k <gigorasd@gmail.com>
2021-08-25 00:10:23 +0300

    init: start expression-calculator task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let begin = 0;
+    let end = 0;
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === '(') begin++;
+        else if (expr[i] === ')') end++;
+    }
+    if (begin !== end) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    } else {
+        let a = new Function(`return ${expr}`);
+        let b = a();
+        if (b === Infinity) {
+            throw new Error('TypeError: Division by zero.');
+        }
+        return b;
+    }
 }
 
 module.exports = {
